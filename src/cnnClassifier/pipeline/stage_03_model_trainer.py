aee48feee68b31be02b70d905788bc609c18a213
Haz <sarchaure911@gmail.com>
2024-01-05 23:16:33 +0530

    prediction pipeline and flask done

--- src/cnnClassifier/pipeline/stage_03_model_trainer.py
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -1,6 +1,8 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_trainer import Training
 from cnnClassifier import logger
+import shutil
+import os
 
 
 STAGE_NAME ='Training'
@@ -16,6 +18,13 @@ class ModelTrainingPipeline:
         training.get_base_model()
         training.train_valid_generator()
         training.train()
+        destination_directory="model"
+        file_path=r"artifacts/training/model.h5"
+        if not os.path.exists(destination_directory):
+            os.makedirs(destination_directory)
+        shutil.copy(file_path, destination_directory)
+        print("Model copied to Model directory")
+        
 
 if __name__ =='__main__':
     try:
